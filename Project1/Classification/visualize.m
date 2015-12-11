5a5df9304df1c02cb0f5feee75897108ad1294df
Martin Engilberge <martin.engilberge@gmail.com>
2015-12-11 13:07:56 +0100

    Project 2 start

--- Project1/Classification/visualize.m
+++ Project1/Classification/visualize.m
@@ -21,10 +21,16 @@ for i=1:size(X_true,2)
     %scatter(X_train(:,i),y_train, '.');
     histogram(X_true(:,i))
     hold on
-    histogram(X_false(:,i))
     
-    hx = xlabel('Value taken by a single feature');
-    hy = ylabel('Number of value');
+    idx1 = find(y_train == 1);
+    idx0 = find(y_train == 0);
+    figure
+    histogram(y_train(idx1,:))
+     hold on
+     histogram(y_train(idx0,:))
+     
+    hx = xlabel('Class');
+    hy = ylabel('Number of element');
     
     % the following code makes the plot look nice and increase font size etc.
     set(gca,'fontsize',20,'fontname','Helvetica','box','off','tickdir','out','ticklength',[.02 .02],'xcolor',0.5*[1 1 1],'ycolor',0.5*[1 1 1]);
