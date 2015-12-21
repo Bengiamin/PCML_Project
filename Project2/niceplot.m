3342d57428e302bbd8265b38ddd9ef1de01f32a6
Bengiamin Barblan <bengiamin.barblan@epfl.ch>
2015-12-21 10:47:05 +0100

    "niceplot "

--- Project2/niceplot.m
+++ Project2/niceplot.m
@@ -1,9 +1,16 @@
 	
-    values = xvals;
-    hx = xlabel('x');
-	hy = ylabel('y');
-	ht = title(sprintf('Polynomial degree %d',degree));
+
+    hx = xlabel('First x principal components');
+	hy = ylabel('BER');
+	ht = title(sprintf('BER for SVM with the first x PC'));
     
-    plot(values, f,'r-','linewidth',2);
-	xlim([min(xvals), max(xvals)]);
+    plot(berMeanTest(2,:), berMeanTest(1, :), 'r-','linewidth',2);
+    hold on
+    plot(berMeanTrain(2, :), berMeanTrain(1, :),'b-','linewidth',2);
+    
+    
+    legend('BER test', 'BER train');
+    
+	ylim([0.05, 0.2]);
+    xlim([0, 50]);
 	set([hx, hy, ht], 'fontsize', 14);
